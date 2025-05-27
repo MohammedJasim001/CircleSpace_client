46c85dea2f9efa9ebd7a3e388b66ccf371f72c9e
MohammedJasim001 <jasimat001@gmail.com>
2025-05-27 10:27:08 +0530

    final deploy

--- src/hooks/useSockets.ts
+++ src/hooks/useSockets.ts
@@ -3,7 +3,7 @@ import { Message } from '@/types/message';
 import { useState, useEffect, useCallback } from 'react';
 import { io, Socket } from 'socket.io-client';
 
-const SOCKET_URL = "http://localhost:5010";
+const SOCKET_URL = process.env.NEXT_PUBLIC_SOCKET_URL;
 
 export const useSocket = (
   currentUserId: string | null, 
--- src/utils/socket.ts
+++ src/utils/socket.ts
@@ -1,6 +1,7 @@
 import { io, Socket } from "socket.io-client";
 
-const SOCKET_URL = process.env.SOCKET_URL; 
+const SOCKET_URL = process.env.NEXT_PUBLIC_SOCKET_URL; 
+console.log(SOCKET_URL,'asdfasdf');
 
 let socket: Socket | null = null;
 
